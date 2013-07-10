d0240ae789ac09c4dd3dfa5d1d76374e4a0ed8f6
Ed Schmalzle <ed@nerded.net>
2013-07-10 14:31:50 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BmoreonrailsOrg::Application.config.secret_key_base = 'c752aa63382ae9494e9512333b8f9d3a56cf3b46f379e83373e7b38b2c789f3bf62afc572afe5ea4012135135530f8a33313ede170359ad9935054a5a6a9e3cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BmoreonrailsOrg::Application.config.session_store :cookie_store, key: '_bmoreonrails_org_session'

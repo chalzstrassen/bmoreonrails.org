4cc4c16182894109238fe3723eb9be35a7ee9a10
Hadley Robin <hadley@inthebackforty.com>
2013-07-16 17:20:09 -0400

    added twitter feed and general tweaking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # Returns true if the current request is made from a mobile browser
+  def mobile?
+    return @mobile if defined? @mobile
+    @mobile = request.user_agent =~ /palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|mobile/i
+    @mobile = @mobile && request.user_agent !~ /iPad/
+  end
 end

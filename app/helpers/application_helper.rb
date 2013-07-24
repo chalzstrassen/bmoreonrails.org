3468b42eb63eceb2ba220a1c9baf9c3437b68787
Douglas Price <solipet@gmail.com>
2013-07-24 07:18:14 -0400

    Removed the unused method from application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,4 @@ module ApplicationHelper
     @mobile = request.user_agent =~ /palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|mobile/i
     @mobile = @mobile && request.user_agent !~ /iPad/
   end
-
-  def member_pixel(index)
-    member = Member.member(index)
-    
-  end
 end

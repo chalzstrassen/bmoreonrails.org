26d2bb60cf0fe1d709661926ab27e4e894d0c264
Jonathan Julian <jonathan.m.julian@gmail.com>
2013-08-27 19:24:01 -0400

    Hard-coded member images in images/members, grab the list and shuffle for front-page display

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @member_images = Dir.entries(Rails.root.join('app','assets','images','members')).select {|fname| fname !~ /^\./ }.map {|fname| "members/#{fname}" }.shuffle
     @meetups = Meetup.most_recent
   end
 end

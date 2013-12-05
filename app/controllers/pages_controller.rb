89d22cb0aeb26ef2431a8dea4927b98cb907adcc
Ed Schmalzle <ed@nerded.net>
2013-12-05 15:49:42 -0500

    Show 3 most recent in ascending chron. order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def home
     @members = Member.list.shuffle
     @member_images = @members.map {|member| "members/#{member.avatar_file}" }
-    @meetups = Meetup.most_recent
+    @meetups = Meetup.most_recent.reverse
   end
 end

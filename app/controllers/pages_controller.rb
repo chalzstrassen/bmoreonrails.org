0de2863e409e29d9e778842d2e9a9960fdcdac52
Douglas Price <solipet@gmail.com>
2014-01-02 10:51:13 -0500

    Restored the hover tooltip and link to twitter page for member avatars.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
   def home
     @members = Member.list.shuffle
-    @member_images = @members.map {|member| "members/#{member.avatar_file}" }
     @meetups = Meetup.most_recent.reverse
   end
 end

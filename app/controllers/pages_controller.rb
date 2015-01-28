4cd48ab9c2722fafecf2d2dca45ad1848fdd1307
Natasha Jones <tasha.k.jones@gmail.com>
2015-01-27 20:02:33 -0500

    add list weighting for diversity

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
   def home
-    @members = Member.list.shuffle
+    @members = weighted_shuffle(Member.list)
     @meetups = Meetup.upcoming.limit(3)
+  end
+
+  def weighted_shuffle(array)
+    array.sort_by { |person|
+      weight = person[:list_weight] || 0.3
+      weight * rand
+    }
   end
 end
